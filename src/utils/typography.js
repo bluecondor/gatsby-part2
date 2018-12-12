7da8b6ae70163ee794a9d4a75f691d61f9f97614
Petru Conduraru <petruconduraru@outlook.com>
2018-12-12 21:06:05 +0100

    added typography-theme-twin-peaks

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,8 @@
 import Typography from "typography"
+//import bootstrapTheme from "typography-theme-bootstrap"
+//import lawtonTheme from "typography-theme-lawton"
+import twinPeaks from "typography-theme-twin-peaks"
 
-const typography = new Typography({ baseFontSize: "24px" })
+const typography = new Typography(twinPeaks)
 
 export default typography
